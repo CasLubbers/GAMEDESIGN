996e8b7175994156a8de168924616b158619dd96
Cas Lubbers <Cas_9@live.nl>
2019-02-18 17:21:59 +0100

    Initial game in wessels view

---
+++ Workshop2/Assets/Scripts/DropPickUp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropPickUp : MonoBehaviour
+{
+    public Material playerMaterial;
+    public GameObject box;
+    public GameObject cylinder;
+    public GameObject sphere;
+    public GameObject ground;
+
+
+    private int count = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if(count == 0)
+            {
+                ground.gameObject.GetComponent<Renderer>().material.SetColor("_Color", other.gameObject.GetComponent<Renderer>().material.color);
+            }
+            if (count == 1)
+            {
+                box.gameObject.GetComponent<Renderer>().material.SetColor("_Color", other.gameObject.GetComponent<Renderer>().material.color);
+            }
+            if (count == 2)
+            {
+                cylinder.gameObject.GetComponent<Renderer>().material.SetColor("_Color", other.gameObject.GetComponent<Renderer>().material.color);
+            }
+            if (count == 3)
+            {
+                sphere.gameObject.GetComponent<Renderer>().material.SetColor("_Color", other.gameObject.GetComponent<Renderer>().material.color);
+            }
+            other.gameObject.GetComponent<Renderer>().material.SetColor("_Color", playerMaterial.color);
+            count++;
+        }
+    }
+}
